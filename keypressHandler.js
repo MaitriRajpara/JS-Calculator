b354a6e47589b43c543dd4b1da5ed518ac0eebd1
MaitriRajpara <maitri.rajpara@simformsolutions.com>
2025-03-31 14:39:29 +0530

    Add: keyboard listeners

--- keypressHandler.js
+++ keypressHandler.js
@@ -1,23 +1,35 @@
+// keyboard-handler
+
 export function handleKeyPress(event) {
     const key = event.key;
 
-    if (!isNaN(key)) {
-        this.appendValue(key); // Append number
-    } else if (key === '+') {
-        this.add();
-    } else if (key === '-') {
-        this.subtract();
-    } else if (key === '*') {
-        this.multiply();
-    } else if (key === '/') {
-        this.divide();
-    } else if (key === '.') {
-        this.decimal();
-    } else if (key === 'Enter') {
-        this.result(); // Calculate result
-    } else if (key === 'Backspace') {
-        this.backspace(); // Remove last entry
-    } else if (key === 'Escape') {
-        this.clearDisplay(); // Clear the display
+    switch (key) {
+        case '+':
+            this.add();
+            break;
+        case '-':
+            this.subtract();
+            break;
+        case '*':
+            this.multiply();
+            break;
+        case '/':
+            this.divide();
+            break;
+        case 'Enter':
+            this.result();
+            break;
+        case 'Backspace':
+            this.backspace();
+            break;
+        case 'Escape':
+        case 'c':
+            this.clearDisplay();
+            break;
+        default:
+            if (!isNaN(key)) {
+                this.appendValue(key);
+            }
+            break;
     }
 }
